b6943dd393e74569038fa35ea25b0e935a0b54af
Sina <sina.majdieh@gmail.com>
2019-12-19 08:29:53 -0500

    make SeperateSeeParam function to seperate the see command params

--- unmarshal.go
+++ unmarshal.go
@@ -1,5 +1,11 @@
 package rcss
 
+import (
+	"fmt"
+
+	chewxySexp "github.com/chewxy/chexySexp"
+)
+
 func ProcessInit(match Match, msg Init, team Team) {
 	team.SetSide(msg.Side)
 	team.SetPlayMode(msg.PlayMode)
@@ -19,4 +25,40 @@ func ProcessHear(m Hear, team Team) {
 		team.Kickoff()
 	}
 
+}
+func SeperateSeeParam(m *See, str string) {
+	Sexp, err := chewxySexp.ParseString(str)
+	if err != nil {
+		fmt.Println("See Error : ", err)
+	}
+	child := fmt.Sprint(Sexp[0].Tail())
+	Sexp, err = chewxySexp.ParseString(child)
+	for child != "()" {
+		head := fmt.Sprint(Sexp[0].Head())
+		fmt.Println(head)
+		m.see.Array = append(m.see.Array, head)
+		child = fmt.Sprint(Sexp[0].Tail())
+		fmt.Println(child)
+		Sexp, err = chewxySexp.ParseString(child)
+
+	}
+	fmt.Println(m.see.Array)
+}
+func ProcessSee(obj string) {
+	Sexp, err := chewxySexp.ParseString(obj)
+	if nil != err {
+		fmt.Printf("Error : %s\n", err)
+	}
+	head := fmt.Sprint(fmt.Sprint(Sexp[0].Head()))
+	Sexp, err = chewxySexp.ParseString(head)
+	if nil != err {
+		panic(err)
+	}
+	head = fmt.Sprint(Sexp[0].Head())
+
+	if head == "f" {
+		fmt.Println("flag")
+	} else {
+		fmt.Println(head)
+	}
 }
